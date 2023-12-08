6cdce81f25748fa46ad854a617b7efea175e1233
tubamouqeem <tubamouqeem@gmail.com>
2023-12-09 04:08:37 +0500

    I am still working on it

--- stringTruncation.js
+++ stringTruncation.js
@@ -31,8 +31,10 @@ let fruit = "Bana\'na"
 console.log(fruit)
 
 //Real work
-let content = `Truncate better if the message contains a single very
- long word Instead of truncating at a word boundary, you can truncate at a specific character limit. 
+let content = `https://www.example.com/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+Truncate better if the message contains a single very
+ long word Instead of truncating at a word boundary, you can truncate at a specific https://www.youtube.com/watch?v=8lGpZkjnkt4
+ character limit. 
  This would handle cases where a single very long word exceeds the limit. For example, if you want to 
  limit the content to 150 characters, you can do:`
 
@@ -41,8 +43,17 @@ let content = `Truncate better if the message contains a single very
 const characterLimit = 150;
 function truncateString1(str, characterLimit) {
 
-    if (content.length > characterLimit)
-        return content.slice(0, characterLimit - 3) + "..."
+    // if (content.length > characterLimit)
+    //     return content.slice(0, characterLimit - 3) + "..."
+    if (content.length > 150) {                
+        // Truncate content at a word boundary 
+        for (i = 150; i > 0; i -= 1) {         
+            if (content[i] === " ") {          
+                break;                         
+            }                                  
+        }                                      
+                  
+    }  return  content = content.slice(0, characterLimit)+ " [...]"; 
 }
 
 console.log(truncateString1(content, characterLimit))
