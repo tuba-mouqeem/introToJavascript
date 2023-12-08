d649c67326d12272690deabdd27a4bd003ed67d9
tubamouqeem <tubamouqeem@gmail.com>
2023-12-09 02:21:51 +0500

    One tutorial completed

--- stringTruncation.js
+++ stringTruncation.js
@@ -1,10 +1,15 @@
 function truncateString(str, num){
     //Clear out that junk in your trunk
-    if(num<=3) return str.slice(0,num)+"..."
-    return str.slice(0,num)
+    if (num>= str.length) return str;
+    if(num<3) return str.slice(0,num)+"..." +"I am less than 3 called";
+    return str.slice(0,num-3)+"...";
 }
 
-truncateString("A-tisket a-tasket A green and yellow backet", 11)
-truncateString("Tommy goes to work", 4)
+console.log(truncateString("A-tisket a-tasket A green and yellow backet", 11))
+console.log(truncateString("Tommy goes to work", 5))
+console.log(truncateString("tes",2))
+
+
+
 var name="jonathan";
 console.log(name.slice(0,2))
